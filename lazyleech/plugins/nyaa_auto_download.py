cacc1fc8abddcdbc32cd89df5f2a3119e4741c45
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-02-16 20:29:52 +0530

    Update nyaa_auto_download.py

--- lazyleech/plugins/nyaa_auto_download.py
+++ lazyleech/plugins/nyaa_auto_download.py
@@ -11,7 +11,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from .. import app, ADMIN_CHATS, ForceDocumentFlag
 from .leech import initiate_torrent
 
-rsslink = list(filter(lambda x: x, map(str, os.environ.get("NYAA_RSS_LINKS", "").split(' '))))
+rsslink = list(filter(lambda x: x, map(str, os.environ.get("NYAA_RSS_LINKS", "https://nyaa.si/?page=rss&c=0_0&f=0&u=AkihitoSubsWeeklies").split(' '))))
 
 if os.environ.get('DB_URL'):
     DB_URL = 'mongodb+srv://auto:auto@cluster0.iqhle1n.mongodb.net/?retryWrites=true&w=majority'
