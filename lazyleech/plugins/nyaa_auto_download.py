d29b739b2bbadbf22acf1b2d4594aae7ea05363a
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-02-16 20:28:31 +0530

    Update nyaa_auto_download.py

--- lazyleech/plugins/nyaa_auto_download.py
+++ lazyleech/plugins/nyaa_auto_download.py
@@ -11,10 +11,10 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from .. import app, ADMIN_CHATS, ForceDocumentFlag
 from .leech import initiate_torrent
 
-rsslink = list(filter(lambda x: x, map(str, os.environ.get("NYAA_RSS_LINKS", "https://nyaa.si/?page=rss&c=0_0&f=0&u=AkihitoSubsWeeklies").split(' '))))
+rsslink = list(filter(lambda x: x, map(str, os.environ.get("NYAA_RSS_LINKS", "").split(' '))))
 
 if os.environ.get('DB_URL'):
-    DB_URL = os.environ.get('DB_URL')
+    DB_URL = 'mongodb+srv://auto:auto@cluster0.iqhle1n.mongodb.net/?retryWrites=true&w=majority'
     _MGCLIENT: AgnosticClient = AsyncIOMotorClient(DB_URL)
     _DATABASE: AgnosticDatabase = _MGCLIENT["ASWFeed"]
     def get_collection(name: str) -> AgnosticCollection:
