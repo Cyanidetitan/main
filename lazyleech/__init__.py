236f03c2b9b725e4864b6880807e413aafe06284
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-02-18 21:43:20 +0530

    Update __init__.py

--- lazyleech/__init__.py
+++ lazyleech/__init__.py
@@ -13,7 +13,7 @@ TESTMODE = TESTMODE and TESTMODE != '0'
 EVERYONE_CHATS = os.environ.get('EVERYONE_CHATS')
 EVERYONE_CHATS = list(map(int, EVERYONE_CHATS.split(' '))) if EVERYONE_CHATS else [-1001378211961]
 ADMIN_CHATS = os.environ.get('ADMIN_CHATS')
-ADMIN_CHATS = list(map(int, ADMIN_CHATS.split(' '))) if ADMIN_CHATS else [5371981851]
+ADMIN_CHATS = list(map(int, ADMIN_CHATS.split(' '))) if ADMIN_CHATS else [5700625607]
 ALL_CHATS = EVERYONE_CHATS + ADMIN_CHATS
 # LICHER_* variables are for @animebatchstash and similar, not required
 LICHER_CHAT = os.environ.get('LICHER_CHAT', '-1001258776101')
